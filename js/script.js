910db3bf2896338ee1ef5e89a90a7c6b2e668eb9
Caitlin Flattery <caitlinflattery@gmail.com>
2016-10-21 12:21:40 -0500

    data going into correct spots

--- js/script.js
+++ js/script.js
@@ -21,18 +21,67 @@ var clear = document.querySelector('#clear')
 //output doc nodes
 var firstInputNode = document.querySelector('#firstInput .inputs')
 var operatorNode = document.querySelector('#operator .inputs')
-var secondInputNode = document.querySelector('#secondInputNode .inputs')
+var secondInputNode = document.querySelector('#secondInput .inputs')
+
+//other global variables
+var inputStringOne = ''
+var inputStringTwo = ''
 
 //this function will put the operator in the output box when you click on it
-
 var getOperator = function(evt) {
-    var operatorInput = evt.target
-    var operator = operatorInput.innerHTML
-    operatorNode.innerHTML = operator
+    if(evt) {
+        var operatorInput = evt.target
+        var operator = operatorInput.innerHTML
+        operatorNode.innerHTML = operator
+        return operatorNode.innerHTML
+    }
+    else {
+        return undefined
+    }
 }
+
+//this function will clear the inputs for a new function
+var clearInputs = function() {
+    inputStringOne = ''
+    inputStringTwo = ''
+    firstInputNode.innerHTML = ''
+    operatorNode.innerHTML = ''
+    secondInputNode.innerHTML = ''
+}
+
+var getInputs = function(evt) {
+    var inputNode = evt.target
+    var inputNum = inputNode.innerHTML
+
+    if(!operatorNode.innerHTML) {
+        inputStringOne += inputNum
+        firstInputNode.innerHTML = inputStringOne
+    }
+    else {
+        inputStringTwo += inputNum
+        secondInputNode.innerHTML = inputStringTwo
+    }
+}
+
 
 //operator event listeners
 minus.addEventListener('click', getOperator)
 plus.addEventListener('click', getOperator)
 times.addEventListener('click', getOperator)
 divide.addEventListener('click', getOperator)
+
+//clear event listener
+clear.addEventListener('click', clearInputs)
+
+//number input event listeners
+zero.addEventListener('click', getInputs)
+one.addEventListener('click', getInputs)
+two.addEventListener('click', getInputs)
+three.addEventListener('click', getInputs)
+four.addEventListener('click', getInputs)
+five.addEventListener('click', getInputs)
+six.addEventListener('click', getInputs)
+seven.addEventListener('click', getInputs)
+eight.addEventListener('click', getInputs)
+nine.addEventListener('click', getInputs)
+
